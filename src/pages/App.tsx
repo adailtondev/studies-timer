345201d62538133f4bd4aae15525839b7864dea4
adailtondev <adailtonh.dev@gmail.com>
2023-05-02 12:47:23 -0300

    visual page done

---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import style from './App.module.scss'
+import Cronometro from '../components/Cronometro';
+
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Formulario />
+      <Lista />
+      <Cronometro />
+    </div>        
+  );  
+}
+
+export default App;
